6273d6ff237e104f51cf2ccab8f17cda06c98226
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-06-14 14:05:27 -0300

    Intro PHP

---
+++ intro_php/index.php
@@ -0,0 +1,25 @@
+<!-- Chave de abertura -->
+<?php
+// O echo serve para imprimir algo na tela
+// echo '<h1>Hello World!!!</h1>';
+
+// Variáveis
+$name = 'Anderson';
+$age = 30;
+$isReal = true;
+
+// $name = 21;
+$isReal = false;
+
+// $valor = 0;
+// var_dump($valor);
+
+// if($valor){
+//     echo "<p>Entrou</p>";
+// }else{
+//     echo "<p>Não entrou</p>";
+// }
+
+$name = 'Anderson';
+
+echo "<h1>Bem-vindo ao meu sistema $name</h1>";
