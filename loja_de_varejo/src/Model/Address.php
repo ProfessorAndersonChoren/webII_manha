a231305258c1bef7b056d943e179df227ffde0e8
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-06-22 11:58:02 -0300

    Adição dos métodos as classes

---
+++ loja_de_varejo/src/Model/Address.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace APP\Model;
+
+class Address
+{
+    private string $publicPlace;
+    private int $streetNumber;
+    private string $neighborhood;
+    private string $city;
+    private string $postalCode;
+}
---
+++ loja_de_varejo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4430d759674aa847714420eba8b414b9524832e2',
+        'name' => 'anderson/loja_de_varejo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'anderson/loja_de_varejo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4430d759674aa847714420eba8b414b9524832e2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
