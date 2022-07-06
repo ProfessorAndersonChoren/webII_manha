db2f8757bd75ad7dbf4dd4da8108e4651948f3c1
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-07-06 10:43:52 -0300

    Classe provider

--- loja_de_varejo/src/Model/Validation.php
+++ loja_de_varejo/src/Model/Validation.php
@@ -1,20 +1,21 @@
 <?php
-
 namespace APP\Model;
 
 class Validation
 {
-    public static function validateName(string $name): bool
+    public static function validateName(string $name):bool
     {
         return mb_strlen($name) > 4;
     }
-
     public static function validateNumber(int|float $value)
     {
         return $value > 0;
     }
-
-    public static function validateBarCode(string $barCode):bool
+    public static function validateCnpj(int $cnpj):bool
+    {
+        return mb_strlen($cnpj) == 14;
+    }
+    public static function validateBarCode(int $barCode):bool
     {
         return mb_strlen($barCode) == 13 && mb_substr($barCode, 0, 3) == '789';
     }
